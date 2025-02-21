11fc285cfb5f27fac6e2ee4f1e1bd470cd4348f0
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-02-21 23:33:05 +0530

    created User model

---
+++ src/models/Users.js
@@ -0,0 +1,10 @@
+import mongoose, {model, models, Schema} from "mongoose";
+
+const userSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    role: {type: String, enum: ["user", "admin"], default: "user"},
+});
+
+export default mongoose.models.user || mongoose.model("User", userSchema);
