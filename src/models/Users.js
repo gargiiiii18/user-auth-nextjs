cd5d7921792a6038587617a61157acba0b0f27fb
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-02-23 23:01:25 +0530

    updated model

--- src/models/Users.js
+++ src/models/Users.js
@@ -2,10 +2,14 @@ import mongoose, {model, models, Schema} from "mongoose";
 
 const userSchema = new Schema({
     name: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
+    email: {type: String, required: [true, 'Please provide an email'], unique: true},
+    password: {type: String, required: [true, 'Please provide a password']},
     role: {type: String, enum: ["user", "admin"], default: "user"},
-    isVerfified: {type:Boolean, default: false}
+    isVerfified: {type:Boolean, default: false},
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
 });
 
 export default mongoose.models.user || mongoose.model("User", userSchema);
