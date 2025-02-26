bac722779277a14bc300b65a02a3d7daf4faa20a
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-02-26 21:06:47 +0530

    updated

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -2,36 +2,48 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { Axios } from "axios";
-import { log } from "util";
+import axios from "axios";
+import toast, {Toaster} from "react-hot-toast";
 
 export default function SignupPage(){
 
     const router = useRouter();
+    // const notify = () => {
+    //     toast("Here's your toast!");
+    // }
 
     const [userCreds, setUserCreds] = useState({
+        username: "",
         email: "",
         password: "",
-        username: "",
     });
     const [buttonDisabled, setButtonDisabled] = useState(true);
 
     const handleSignup = async () => {
        try {
-         
+         const response = await axios.post("/api/users/signup", userCreds);
+         console.log(response.data);
+         toast.success(response.data.message);
+         router.push("/login");
        } catch (error) {
         console.log(error);
+        if(error.status == 400){
+            toast.error("User already exists.")
+        } else{
+            toast.error("Internal server error.");  
+        }
        }
     }
 
     useEffect(() => {
         if(userCreds.email.length>0 && userCreds.password.length>0 && userCreds.username.length>0){
-            setButtonDisabled(!buttonDisabled);
+            setButtonDisabled(false);
         }
     }, [userCreds]);
 
     return(
         <div>
+        <Toaster/>
         <h1 className="m-2 p-2 text-4xl font-semibold text-center">Signup</h1>
         <div className="flex w-auto flex-col gap-5 items-center">
         <div className="flex">
