32390cef6cfaf2586afcb976e0024e3bc4110758
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-02-27 23:04:39 +0530

    craeted middleware

---
+++ src/middleware.js
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from "next-auth/jwt";
+export { default } from "next-auth/middleware";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request) {
+
+  const token = await getToken();
+  const url = request.nextUrl;
+  if(token && 
+    (
+      url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname.startsWith("/")
+    )
+  ){
+  return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+
+  return NextResponse.redirect(new URL('/home', request.url))
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/sign-in',
+    '/sign-up',
+    '/',
+  ] 
+}
+
