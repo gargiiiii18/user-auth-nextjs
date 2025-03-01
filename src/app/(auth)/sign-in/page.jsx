fd23b18899473c5a75df957696103ef2fc9e2049
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-03-02 00:13:04 +0530

    updated

--- src/app/(auth)/sign-in/page.jsx
+++ src/app/(auth)/sign-in/page.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+"use client";
+import { useSession } from "next-auth/react"
 
-const page = () => {
-  return (
+export default function Component() {
+  const { data: session, status } = useSession()
+
+  const signIn = () => {
+    console.log("tata");
+    
+  }
+
+  if (status === "authenticated") {
+    return <p>Signed in as {session.user.email}</p>
+  }
+
+  // return <a href="/api/auth/signin">Sign in</a>
+  return(
     <div>
-      Hello
+      <p>You're not signed in.</p>
+      <button onClick={signIn()} className="px-3 py-1 m-2 bg-blue-600 text-white rounded-lg">Sign In</button>
     </div>
   )
 }
-
-export default page
