a53c818385576851e19a52461f1ea4488bea3d84
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-03-03 21:25:52 +0530

    email logic

---
+++ src/helpers/sendEmail.js
@@ -0,0 +1,17 @@
+import { resend } from "../lib/resend";
+import VerificationEmail from "../../emails/VerificationEmails";
+
+export async function sendVerificationEmail(username, verifyCode) {
+    try {
+        const { data, error } = await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject: 'Verification Code',
+            react: VerificationEmail({username, otp: verifyCode}),
+          });
+        return {success: true, message: 'Verification mail sent successfully.'}    
+    } catch (error) {
+        console.log("email error");
+        return {success: false, message: 'Failed to send verification email.', error}    
+    }
+}
