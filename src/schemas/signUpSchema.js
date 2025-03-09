dfbfda8640240b0670db6a9f715a87289e5a93f6
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-03-09 19:11:36 +0530

    updated route

--- src/schemas/signUpSchema.js
+++ src/schemas/signUpSchema.js
@@ -4,7 +4,7 @@ export const usernameValidation = z
     .string()
     .min(2, "Username must be atleast of 2 characters")
     .max(20,"Username can me of maximum 20 characters")
-    .regex(/^[a-zA-Z0-9_]+$/, "Usrename must not contain any special characters")
+    .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain any special characters")
 
 export const SignUpSchema = z.object({
     username: usernameValidation,
